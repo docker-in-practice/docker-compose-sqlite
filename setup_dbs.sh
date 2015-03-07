d475cb1fab9d2d2d9be00660cc5a44a5066c0cb8
ianmiell <ian.miell@gmail.com>
2015-03-07 11:03:58 +0000

    versions

--- setup_dbs.sh
+++ setup_dbs.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 echo "Creating directory"
+SQLITE=sqlite
+if [[ $(which sqlite) ]]
+then
+	SQLITE=sqlite
+elif [[ $(which sqlite2) ]]
+then
+	SQLITE=sqlite2
+elif [[ $(which sqlite3) ]]
+then
+	SQLITE=sqlite3
+else
+	echo "Please install sqlite"
+	exit 1
+fi
 SQLITEDIR=/tmp/sqlitedbs
 rm -rf $SQLITEDIR
 if [ -a $SQLITEDIR ]
@@ -10,10 +24,10 @@ fi
 mkdir -p $SQLITEDIR
 cd $SQLITEDIR
 echo "Creating DBs"
-echo 'create table t1(c1 text);' | sqlite test
-echo 'create table t1(c1 text);' | sqlite live
+echo 'create table t1(c1 text);' | $SQLITE test
+echo 'create table t1(c1 text);' | $SQLITE live
 echo "Inserting data"
-echo 'insert into t1 values ("test");' | sqlite test
-echo 'insert into t1 values ("live");' | sqlite live
+echo 'insert into t1 values ("test");' | $SQLITE test
+echo 'insert into t1 values ("live");' | $SQLITE live
 cd - > /dev/null 2>&1 
 echo "All done OK"
